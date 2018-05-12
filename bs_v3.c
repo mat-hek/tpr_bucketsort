fe47e9088c00975e3d55a63f202058b3bb94a977
Bartosz Walkowicz <wallko1@hotmail.com>
2018-05-12 12:39:11 +0200

    minor improvements to bs_v3; improving merge left

--- bs_v3.c
+++ bs_v3.c
@@ -5,7 +5,7 @@
 #include <limits.h>
 #include <stdbool.h>
 
-#define BUCKETS_NUM 10
+#define BUCKETS_NUM 1000
 
 
 typedef struct{
@@ -68,14 +68,14 @@ void shift_heap(HeapNode **heap, int *heap_size){
 }
 
 
-void merge_buckets(int *arr, int arr_len, Bucket **buckets, int threads_num, int rank){
-    HeapNode *heap_nodes = malloc(sizeof(HeapNode) * threads_num);
-    HeapNode **heap = malloc(sizeof(HeapNode *) * threads_num);
+void merge_buckets(int *arr, Bucket **buckets, int *offsets, int threads_num, int rank){
+    HeapNode heap_nodes[threads_num];
+    HeapNode *heap[threads_num];
     Bucket *bucket;
 
     #pragma omp for schedule(dynamic)
     for(int bucket_num = 0; bucket_num < BUCKETS_NUM; ++bucket_num){
-        int offset = 0;
+        int offset = offsets[bucket_num];
         int heap_size = 0;
 
         for(int i = 0; i < threads_num; ++i){
@@ -88,8 +88,6 @@ void merge_buckets(int *arr, int arr_len, Bucket **buckets, int threads_num, int
                 heap[heap_size] = &heap_nodes[heap_size];
                 ++heap_size;
             }
-            for(int j = 0; j < bucket_num; ++j)
-                offset += buckets[i][j].len;
         }
 
         if(heap_size > 0){
@@ -100,83 +98,91 @@ void merge_buckets(int *arr, int arr_len, Bucket **buckets, int threads_num, int
             }
         }
     }
-
-    free(heap_nodes);    
-    free(heap);
     return;
 }
 
 
-int cmp(const void *a, const void *b){    
+int cmp(const void *a, const void *b){
     return *(int*)b - *(int*)a;
 }
 
 
-void sort_buckets(Bucket **buckets, int threads_num){
-    #pragma omp for schedule(dynamic)
-    for(int i = 0; i < BUCKETS_NUM * threads_num; ++i){
-        int row = i / BUCKETS_NUM;
-        int col = i % BUCKETS_NUM;
+static inline void sort_buckets(Bucket **buckets, int *offsets, int threads_num, int buckets_num){
+    #pragma omp for reduction(+:offsets[:buckets_num]) schedule(dynamic)
+    for(int i = 0; i < buckets_num * threads_num; ++i){
+        int row = i / buckets_num;
+        int col = i % buckets_num;
         Bucket *bucket = &buckets[row][col];
-        qsort(bucket->arr, bucket->len, sizeof(int), cmp);
+        int n_elems = bucket->len;
+        for(int j = col + 1; j < buckets_num; ++j)
+            offsets[j] += n_elems;
+        qsort(bucket->arr, n_elems, sizeof(int), cmp);
     }
     return;
 }
 
 
-void fill_buckets(int *arr, int arr_len, Bucket *buckets){
+static inline void fill_buckets(int *arr, int arr_len, Bucket *buckets, int buckets_num){
     Bucket *bucket;
-    int bucket_range = (INT_MAX / BUCKETS_NUM) + 1;
+    int bucket_range = (INT_MAX / buckets_num) + 1;
 
     #pragma omp for schedule(static)
     for(int i = 0; i < arr_len; ++i){
         bucket = &buckets[arr[i] / bucket_range];
-        bucket->arr[bucket->len] = arr[i];
-        bucket->len += 1;
+        bucket->arr[bucket->len++] = arr[i];
     }
     return;    
 }
 
 
-void bucket_sort(int *arr, int arr_len){
-    Bucket **buckets = malloc(sizeof(Bucket *) * omp_get_max_threads());
+static inline Bucket *create_buckets(int bucket_size, int buckets_num){
+    Bucket *buckets = malloc(sizeof(Bucket) * buckets_num);
+    for(int i = 0; i < buckets_num; ++i)
+        buckets[i] = (Bucket) {.len = 0, .arr = malloc(sizeof(int) * bucket_size)};
+    return buckets;
+}
 
-    #pragma omp parallel shared(buckets, arr, arr_len)
-    {
-        int rank = omp_get_thread_num();
-        int threads_num = omp_get_num_threads();
-        int bucket_size = (arr_len / threads_num) + threads_num;
 
-        Bucket *thread_buckets = malloc(sizeof(Bucket) * BUCKETS_NUM);
-        for(int i = 0; i < BUCKETS_NUM; ++i){
-            thread_buckets[i].len = 0;
-            thread_buckets[i].arr = malloc(sizeof(int) * bucket_size);
-        }
-        buckets[rank] = thread_buckets;
-
-        fill_buckets(arr, arr_len, thread_buckets);
-        sort_buckets(buckets, threads_num);
-        merge_buckets(arr, arr_len, buckets, threads_num, rank);
-
-        for(int i = 0; i < BUCKETS_NUM; ++i)
-            free(buckets[rank][i].arr);
-        free(buckets[rank]);
-    }
-
+static inline void free_buckets(Bucket *buckets, int buckets_num){
+    for(int i = 0; i < buckets_num; ++i)
+        free(buckets[i].arr);
     free(buckets);
     return;
 }
 
 
-void fill_array(int *arr, int arr_len){
+void bucket_sort(int *arr, int arr_len, int buckets_num){
+    Bucket *all_buckets[omp_get_max_threads()];
+    int offsets[buckets_num];
+    for(int i = 0; i < buckets_num; ++i)
+        offsets[i] = 0;
+
+    #pragma omp parallel shared(all_buckets, arr, arr_len, buckets_num)
+    {
+        int rank = omp_get_thread_num();
+        int threads_num = omp_get_num_threads();
+
+        Bucket *thread_buckets = create_buckets(arr_len, buckets_num);
+        all_buckets[rank] = thread_buckets;
+
+        fill_buckets(arr, arr_len, thread_buckets, buckets_num);
+        sort_buckets(all_buckets, offsets, threads_num, buckets_num);
+        merge_buckets(arr, all_buckets, offsets, threads_num, rank);
+
+        free_buckets(thread_buckets, buckets_num);
+    }
+    return;
+}
+
+
+static inline void fill_array(int *arr, int arr_len){
     #pragma omp parallel
     {
         int seed = time(NULL) + omp_get_thread_num();
 
         #pragma omp for schedule(static)
-        for(int i = 0; i < arr_len; ++i){
+        for(int i = 0; i < arr_len; ++i)
             arr[i] = rand_r(&seed);
-        }
     }
     return;
 }
@@ -188,19 +194,21 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    int arr_len = atoi(argv[1]);
+    int arr_len = atoll(argv[1]);
+    int buckets_num = BUCKETS_NUM;
     int *arr = malloc(sizeof(int) * arr_len);
 
     fill_array(arr, arr_len);
 //     print_array(arr, arr_len);
-    bucket_sort(arr, arr_len);
+    bucket_sort(arr, arr_len, buckets_num);
 //     print_array(arr, arr_len);
 
-    for(int i = 0; i < arr_len - 1; ++i)
-        if(arr[i] > arr[i + 1]){
-            printf("\nFAILED!!!\n");
-            break;
-        }
+//     for(int i = 0; i < arr_len - 1; ++i)
+//         if(arr[i] > arr[i + 1]){
+//             printf("\nFAILED!!!\n");
+//             free(arr);
+//             return -1;
+//         }
 
     free(arr);
     return 0;
