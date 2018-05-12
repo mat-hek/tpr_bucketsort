3184da97976b6ea8a9f84beaf0a45f702bf475ef
Bartosz Walkowicz <wallko1@hotmail.com>
2018-05-12 16:23:01 +0200

    finish implementation of bs_v3

--- bs_v3.c
+++ bs_v3.c
@@ -5,19 +5,13 @@
 #include <limits.h>
 #include <stdbool.h>
 
-#define BUCKETS_NUM 1000
+#define DEFAULT_BUCKETS_NUM 1000
 
 
 typedef struct{
-    int len;
+    int n_elem;
     int *arr;
 } Bucket;
-
-
-typedef struct{
-    int idx;
-    int *arr;
-} HeapNode;
 
 
 void print_array(int *arr, int arr_len){
@@ -29,72 +23,67 @@ void print_array(int *arr, int arr_len){
 }
 
 
-void insertion_sort(HeapNode **heap, int heap_size){
-    for(int i = 1; i < heap_size; ++i){
-        HeapNode *node = heap[i];
-        int j = i - 1;
-        for(; j >= 0; --j){
-            if(heap[j]->arr[heap[j]->idx] <= node->arr[node->idx]){
-                heap[j + 1] = node;
-                break;
-            }
-            heap[j + 1] = heap[j];
-            heap[j] = node;
+static inline void heapify(Bucket **heap, int *heap_size, int root){
+    int size = *heap_size;
+    if(heap[0]->n_elem < 0 && size > 1){
+        size = --(*heap_size);
+        heap[0] = heap[size];
+    }
+
+    while(root < size){
+        int left = 2 * root + 1;
+        int right = 2 * root + 2;
+        int smallest = root;
+
+        if(left < size && heap[left]->arr[heap[left]->n_elem] < heap[smallest]->arr[heap[smallest]->n_elem])
+            smallest = left;
+        if(right < size && heap[right]->arr[heap[right]->n_elem] < heap[smallest]->arr[heap[smallest]->n_elem])
+            smallest = right;
+
+        if(root != smallest){
+            Bucket *tmp = heap[root];
+            heap[root] = heap[smallest];
+            heap[smallest] = tmp;
+            root = smallest;
         }
-    }
-}
-
-
-void shift_heap(HeapNode **heap, int *heap_size){
-    HeapNode *node;
-    if(heap[0]->idx < 0 && *heap_size > 1){
-        node = heap[0];
-        heap[0] = heap[*heap_size - 1];
-        heap[*heap_size - 1] = node;
-        *heap_size -= 1;
-    }
-
-    node = heap[0];
-    for(int i = 1; i < *heap_size; ++i){
-        if(heap[i]->arr[heap[i]->idx] >= node->arr[node->idx]){
-            heap[i - 1] = node;
+        else
             break;
-        }
-        heap[i - 1] = heap[i];
-        heap[i] = node;
     }
-
     return;
 }
 
 
-void merge_buckets(int *arr, Bucket **buckets, int *offsets, int threads_num, int rank){
-    HeapNode heap_nodes[threads_num];
-    HeapNode *heap[threads_num];
+static inline void build_heap(Bucket **heap, int heap_size){
+    for(int i = (heap_size / 2) - 1; i >= 0; --i)
+        heapify(heap, &heap_size, i);
+    return;
+}
+
+
+static inline void merge_buckets(int *arr, Bucket **buckets, int *offsets, int threads_num, int buckets_num, int rank){
+    Bucket heap_nodes[threads_num];
+    Bucket *heap[threads_num];
     Bucket *bucket;
 
     #pragma omp for schedule(dynamic)
-    for(int bucket_num = 0; bucket_num < BUCKETS_NUM; ++bucket_num){
+    for(int bucket_num = 0; bucket_num < buckets_num; ++bucket_num){
         int offset = offsets[bucket_num];
         int heap_size = 0;
 
         for(int i = 0; i < threads_num; ++i){
             bucket = &buckets[i][bucket_num];
 
-            int idx = bucket->len - 1;
-            if(idx >= 0){
-                heap_nodes[heap_size].arr = bucket->arr;
-                heap_nodes[heap_size].idx = idx;
-                heap[heap_size] = &heap_nodes[heap_size];
-                ++heap_size;
+            if(--bucket->n_elem >= 0){
+                heap_nodes[heap_size] = *bucket;
+                heap[heap_size++] = &heap_nodes[heap_size];
             }
         }
 
         if(heap_size > 0){
-            insertion_sort(heap, heap_size);
-            while(heap[0]->idx >= 0){
-                arr[offset++] = heap[0]->arr[heap[0]->idx--];
-                shift_heap(heap, &heap_size);
+            build_heap(heap, heap_size);
+            while(heap[0]->n_elem >= 0){
+                arr[offset++] = heap[0]->arr[heap[0]->n_elem--];
+                heapify(heap, &heap_size, 0);
             }
         }
     }
@@ -113,7 +102,7 @@ static inline void sort_buckets(Bucket **buckets, int *offsets, int threads_num,
         int row = i / buckets_num;
         int col = i % buckets_num;
         Bucket *bucket = &buckets[row][col];
-        int n_elems = bucket->len;
+        int n_elems = bucket->n_elem;
         for(int j = col + 1; j < buckets_num; ++j)
             offsets[j] += n_elems;
         qsort(bucket->arr, n_elems, sizeof(int), cmp);
@@ -129,7 +118,7 @@ static inline void fill_buckets(int *arr, int arr_len, Bucket *buckets, int buck
     #pragma omp for schedule(static)
     for(int i = 0; i < arr_len; ++i){
         bucket = &buckets[arr[i] / bucket_range];
-        bucket->arr[bucket->len++] = arr[i];
+        bucket->arr[bucket->n_elem++] = arr[i];
     }
     return;    
 }
@@ -138,7 +127,7 @@ static inline void fill_buckets(int *arr, int arr_len, Bucket *buckets, int buck
 static inline Bucket *create_buckets(int bucket_size, int buckets_num){
     Bucket *buckets = malloc(sizeof(Bucket) * buckets_num);
     for(int i = 0; i < buckets_num; ++i)
-        buckets[i] = (Bucket) {.len = 0, .arr = malloc(sizeof(int) * bucket_size)};
+        buckets[i] = (Bucket) {.n_elem = 0, .arr = malloc(sizeof(int) * bucket_size)};
     return buckets;
 }
 
@@ -167,7 +156,7 @@ void bucket_sort(int *arr, int arr_len, int buckets_num){
 
         fill_buckets(arr, arr_len, thread_buckets, buckets_num);
         sort_buckets(all_buckets, offsets, threads_num, buckets_num);
-        merge_buckets(arr, all_buckets, offsets, threads_num, rank);
+        merge_buckets(arr, all_buckets, offsets, threads_num, buckets_num, rank);
 
         free_buckets(thread_buckets, buckets_num);
     }
@@ -189,13 +178,13 @@ static inline void fill_array(int *arr, int arr_len){
 
 
 int main(int argc, char *argv[]){
-    if(argc != 2){
-        printf("Please insert the size of array");
+    if(argc < 2 || argc > 3){
+        printf("Please insert only the size of array and optionally buckets number");
         return -1;
     }
 
-    int arr_len = atoll(argv[1]);
-    int buckets_num = BUCKETS_NUM;
+    int arr_len = atoi(argv[1]);
+    int buckets_num = argc == 3 ? atoi(argv[2]) : DEFAULT_BUCKETS_NUM;
     int *arr = malloc(sizeof(int) * arr_len);
 
     fill_array(arr, arr_len);
@@ -203,12 +192,12 @@ int main(int argc, char *argv[]){
     bucket_sort(arr, arr_len, buckets_num);
 //     print_array(arr, arr_len);
 
-//     for(int i = 0; i < arr_len - 1; ++i)
-//         if(arr[i] > arr[i + 1]){
-//             printf("\nFAILED!!!\n");
-//             free(arr);
-//             return -1;
-//         }
+    for(int i = 0; i < arr_len - 1; ++i)
+        if(arr[i] > arr[i + 1]){
+            printf("\nFAILED!!! %d %d\n", arr[i], arr[i+1]);
+            free(arr);
+            return -1;
+        }
 
     free(arr);
     return 0;
